71a1d7d45d73664a305999b8315eee0ce5f1356b
raphael sena <rsenares1@gmail.com>
2023-07-07 00:05:46 -0300

    Styarting to implement

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,20 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null; //por padrão o java já joga o valor nulo.
+		
+	}
 
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+	
 }
