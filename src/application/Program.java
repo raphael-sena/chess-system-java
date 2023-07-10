27d99bafb3e4de1867dfbbc450341f463b8d7207
raphael sena <rsenares1@gmail.com>
2023-07-10 17:15:54 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -14,16 +15,23 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.println("Source: ");
-			ChessPosition source = UI.readChessPPosition(sc);
-			
-			System.out.println();
-			System.out.println("Target: ");
-			ChessPosition target = UI.readChessPPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMovo(source, target);
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.println("Source: ");
+				ChessPosition source = UI.readChessPPosition(sc);
+				
+				System.out.println();
+				System.out.println("Target: ");
+				ChessPosition target = UI.readChessPPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMovo(source, target);	
+			}
+			catch(ChessException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 }
